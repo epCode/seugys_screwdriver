13bc929d8d18788fcf2c09f1ea21fc3cfa9f82e4
epCode <eeliperry@gmail.com>
2025-02-09 18:54:38 -0700

    remove uneeded stuff

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
 seugyssd = {}
 
-dofile(core.get_modpath("seugys_screwdriver").."/persistent_effects.lua")
 dofile(core.get_modpath("seugys_screwdriver").."/api.lua")
 dofile(core.get_modpath("seugys_screwdriver").."/abilities.lua")
